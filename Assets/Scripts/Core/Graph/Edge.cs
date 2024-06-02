8cfdaa61157a03bf92538ab0e74cd1047d613d30
Ryan Tan <ryantanzr.work@gmail.com>
2024-06-02 21:09:23 +0800

    feat: added game UI and edge rendering logic

--- Assets/Scripts/Core/Graph/Edge.cs
+++ Assets/Scripts/Core/Graph/Edge.cs
@@ -1,4 +1,7 @@
+using Algowizardry.Utility;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Algowizardry.Core;
 
 /**********************************************************
  * Author: Ryan Tan
@@ -7,7 +10,10 @@ using UnityEngine;
  * represents the cost of traversal and 2 vertices
  *********************************************************/
 
+
 namespace Algowizardry.Core.GraphTheory {
+    
+    [RequireComponent(typeof(LineRenderer))]
     public class Edge : MonoBehaviour
     {
         public Node startVertex;
@@ -30,6 +36,66 @@ namespace Algowizardry.Core.GraphTheory {
             isActive = false;
         }
 
+        public void Start()
+        {
+            LineRenderer lineRenderer = GetComponent<LineRenderer>();
+            MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
+
+            Vector3 startVertexPos = startVertex.transform.position;
+            Vector3 endVertexPos = endVertex.transform.position;
+           
+            lineRenderer.SetPosition(0, startVertexPos);
+            lineRenderer.SetPosition(1, endVertexPos);
+
+            //Generate the mesh in world space
+            Mesh mesh = new Mesh
+            {
+                vertices = new Vector3[]
+                {
+                    startVertexPos,
+                    endVertexPos
+                }
+            };
+
+            lineRenderer.BakeMesh(mesh);
+            
+            meshCollider.sharedMesh = mesh;
+
+        }
+
+        public void Update()
+        {
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch detected");
+                Ray rayFromCamera = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+                if (Physics.Raycast(rayFromCamera, out hit))
+                {
+                    if (hit.collider.gameObject == gameObject)
+                    {
+                        ToggleEdge();
+                    }
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Mouse click detected");
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log("Hit detected" + hit.collider.gameObject.name);
+                    if (hit.collider.gameObject == gameObject)
+                    {
+                        ToggleEdge();
+                    }
+                }
+            }
+        }
+
+
         public void ToggleEdge()
         {
             isActive = !isActive;
@@ -45,5 +111,6 @@ namespace Algowizardry.Core.GraphTheory {
 
         }
 
+        
     }
 }
--- Assets/Scripts/Utility/GameManager.cs
+++ Assets/Scripts/Utility/GameManager.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Algowizardry.Utility
 {
+
     public class GameManager : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        public static GameManager instance;
+        public Camera mainCamera;
+
+        private void Awake()
         {
-            
+            if (instance == null)
+            {
+                instance = this;
+                mainCamera = Camera.main;
+            }
+            else
+            {
+                Destroy(this);
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        public void StartMinigame()
         {
-            
+
+        }
+
+        public void PauseMinigame()
+        {
+
+        }
+
+        public void QuitMinigame()
+        {
+            SceneManager.LoadScene("MainMenu");
         }
     }
 }
